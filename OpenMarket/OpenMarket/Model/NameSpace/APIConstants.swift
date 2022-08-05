0032536076e85bfee2b58af55f0707c6b269a7a0
unchain123 <unchain123@kakao.com>
2022-08-05 14:06:07 +0900

    refactor: APIConstants열거형 정보 수정

--- OpenMarket/OpenMarket/Model/NameSpace/APIConstants.swift
+++ OpenMarket/OpenMarket/Model/NameSpace/APIConstants.swift
@@ -6,7 +6,8 @@
 //
 
 enum APIConstants {
-    static let host = "https://market-training.yagom-academy.kr"
+    static let scheme = "https"
+    static let host = "market-training.yagom-academy.kr"
     static let identifier = "d1fb22fc-0335-11ed-9676-3bb3eb48793a"
     static let passwordKey = "secret"
     static let passwordValue = "lP8VFiBqGI"
