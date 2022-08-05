029070975b445dbc55b82fa9e035d14646a3c7b1
unchain123 <unchain123@kakao.com>
2022-08-05 20:30:09 +0900

    feat: collectionView구현

--- OpenMarket/OpenMarket/ViewController/ProductDetailViewController.swift
+++ OpenMarket/OpenMarket/ViewController/ProductDetailViewController.swift
@@ -33,13 +33,21 @@ final class ProductDetailViewController: UIViewController {
     private var productDetail: SaleInformation?
     private var dataSource: DiffableDataSource?
     private var snapshot = NSDiffableDataSourceSnapshot<Section, SaleInformation>()
- 
+
     private let actionButton: UIButton = {
         let button = UIButton()
         let image = UIImage(systemName: "square.and.arrow.up")
         button.setImage(image, for: .normal)
         button.translatesAutoresizingMaskIntoConstraints = false
         return button
+    }()
+
+    private lazy var collectionView: UICollectionView = {
+        let layout = createGridLayout()
+        let collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: layout)
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        collectionView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        return collectionView
     }()
 
     // MARK: View Life Cycle
@@ -50,11 +58,17 @@ final class ProductDetailViewController: UIViewController {
 
         navigationItem.title = product?.name
         navigationItem.rightBarButtonItem = UIBarButtonItem(customView: actionButton)
-        
+        collectionView.register(GridCollectionViewCell.self, forCellWithReuseIdentifier: CollectionViewNamespace.grid.name)
+        dataSource = configureDataSource(id: CollectionViewNamespace.grid.name)
         self.snapshot.appendSections([.main])
-        
+
+        view.addSubview(collectionView)
+        setCollectionViewConstraint()
         getProductDetail()
+
     }
+
+    // MARK: Method
     
     private func getProductDetail() {
         guard let productId = product?.id else { return }
@@ -74,6 +88,62 @@ final class ProductDetailViewController: UIViewController {
                 DispatchQueue.main.async {
                     self.showCustomAlert(title: nil, message: error.localizedDescription)
                 }
+            }
+        }
+    }
+    
+    private func createGridLayout() -> UICollectionViewCompositionalLayout {
+        let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(Metric.gridItemWidth), heightDimension: .fractionalHeight(Metric.gridItemHeight))
+        let item = NSCollectionLayoutItem(layoutSize: itemSize)
+        
+        let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(Metric.gridGroupWidth), heightDimension: .fractionalHeight(Metric.gridGroupHeight))
+        let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitem: item, count: 1)
+        group.interItemSpacing = .fixed(Metric.gridGroupSpacing)
+        group.contentInsets = NSDirectionalEdgeInsets(top: Metric.padding, leading: Metric.padding, bottom: Metric.padding, trailing: Metric.padding)
+        
+        let section = NSCollectionLayoutSection(group: group)
+        let layout = UICollectionViewCompositionalLayout(section: section)
+        return layout
+    }
+    
+    private func setCollectionViewConstraint() {
+        NSLayoutConstraint.activate([
+            collectionView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            collectionView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            collectionView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+            collectionView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor)
+        ])
+    }
+    
+    private func configureDataSource(id: String) -> DiffableDataSource? {
+        dataSource = DiffableDataSource(collectionView: collectionView) { (collectionView: UICollectionView, indexPath: IndexPath, product: SaleInformation) -> UICollectionViewCell? in
+            
+            guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: CollectionViewNamespace.grid.name, for: indexPath) as? GridCollectionViewCell else { return GridCollectionViewCell() }
+            
+            cell.configureCell(product: product) { result in
+                switch result {
+                case .success(_):
+                    return
+                case .failure(let error):
+                    DispatchQueue.main.async {
+                        self.showCustomAlert(title: nil, message: error.localizedDescription)
+                    }
+                }
+            }
+            return cell
+        }
+        return dataSource
+    }
+
+    func configureCell(product: SaleInformation, completion: @escaping (Result<Data, Error>) -> Void) {
+        guard let url = URL(string: product.thumbnail) else { return }
+
+        NetworkManager().networkPerform(for: URLRequest(url: url)) { [weak self] result in
+            switch result {
+            case .success(let data):
+                guard let images = UIImage(data: data) else { return }
+            case .failure(let error):
+                completion(.failure(error))
             }
         }
     }
