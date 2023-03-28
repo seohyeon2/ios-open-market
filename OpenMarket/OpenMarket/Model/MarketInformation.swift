94219e205f01b3e4cf627122fd0e151873986f82
unchain123 <unchain123@kakao.com>
2023-03-28 23:58:46 +0900

    refactor: DiffableDataSource 크러쉬 문제 해결
    모델에 UUID추가

--- OpenMarket/OpenMarket/Model/MarketInformation.swift
+++ OpenMarket/OpenMarket/Model/MarketInformation.swift
@@ -4,6 +4,9 @@
 //
 //  Created by unchain, hyeon2 on 2022/07/12.
 //
+
+import Foundation
+
 struct MarketInformation: Decodable {
     let pageNo: Int
     let itemsPerPage: Int
@@ -17,35 +20,37 @@ struct MarketInformation: Decodable {
 }
 
 struct PageInformation: Decodable, Hashable {
-        let id: Int
-        let vendorId: Int
-        let vendorName: String
-        let name: String
-        let description: String
-        let thumbnail: String
-        let currency: String
-        let price: Double
-        let bargainPrice: Double
-        let discountedPrice: Double
-        let stock: Int
-        let createdAt: String
-        let issuedAt: String
+    let hashID = UUID()
 
-        enum CodingKeys: String, CodingKey {
-            case id,
-                 name,
-                 description,
-                 thumbnail,
-                 currency,
-                 price,
-                 stock,
-                 vendorName
-            case vendorId = "vendor_id"
-            case bargainPrice = "bargain_price"
-            case discountedPrice = "discounted_price"
-            case createdAt = "created_at"
-            case issuedAt = "issued_at"
-        }
+    let id: Int
+    let vendorId: Int
+    let vendorName: String
+    let name: String
+    let description: String
+    let thumbnail: String
+    let currency: String
+    let price: Double
+    let bargainPrice: Double
+    let discountedPrice: Double
+    let stock: Int
+    let createdAt: String
+    let issuedAt: String
+
+    enum CodingKeys: String, CodingKey {
+        case id,
+             name,
+             description,
+             thumbnail,
+             currency,
+             price,
+             stock,
+             vendorName
+        case vendorId = "vendor_id"
+        case bargainPrice = "bargain_price"
+        case discountedPrice = "discounted_price"
+        case createdAt = "created_at"
+        case issuedAt = "issued_at"
+    }
 }
 
 enum Currency: Int {
